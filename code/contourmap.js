e2466a55cd99fc082a0fceb035eb7ab13c95d7e3
ianalexmac <iamacdougall@alaska.edu>
2024-05-15 08:18:30 -0800

    contour map prep work

--- code/contourmap.js
+++ code/contourmap.js
@@ -21,7 +21,7 @@ const years = [
 
 function filterBy(year) {
   // Set a filter on the heatmap layer to only show data for the selected year
-  map.setFilter('heatmap-layer', ['==', ['get', 'year'], year]);
+  map.setFilter('contour-layer', ['==', ['get', 'year'], year]);
   // Update the year display
   document.getElementById('year-display').textContent = year;
 }
@@ -47,38 +47,23 @@ function jsonCallback(err, data) {
     return d;
   });
 
-  map.addSource('heatmap-source', {
+  map.addSource('contour-source', {
     'type': 'geojson',
     'data': data
   });
 
   map.addLayer({
-    'id': 'heatmap-layer',
-    'type': 'heatmap',
-    'source': 'heatmap-source',
+    'id': 'contour-layer',
+    'type': 'contour',
+    'source': 'contour-source',
     'paint': {
-      // Add custom heatmap layer properties here
-      'heatmap-weight': ['interpolate', ['linear'], ['get', 'Valuation'], 0, 0, 6, 1],
-      'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, 12, 3],
-      'heatmap-color': [
-        'interpolate',
-        ['linear'],
-        ['heatmap-density'],
-        0,
-        'rgba(33,102,172,0)',
-        0.1,
-        'rgb(103,169,207)',
-        0.2,
-        'rgb(209,229,240)',
-        0.5,
-        'rgb(253,219,199)',
-        0.8,
-        'rgb(239,138,98)',
-        1,
-        'rgb(178,24,43)'
-      ],
-      'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 5, 12, 100],
-      'heatmap-opacity': ['interpolate', ['linear'], ['zoom'], 9, 1, 12, 0]
+      'contour-color': [
+        'step',
+        ['get', 'Valuation'],
+        '#f1f075',
+        20,
+        '#eb4d5c'
+      ]
     }
   });
 
--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get('/code/heatmap.js', (req, res) => {
-  fs.readFile(path.join(__dirname, '/code/heatmap.js'), 'utf8', (err, data) => {
+app.get('/code/:filename', (req, res) => {
+  const filename = req.params.filename;
+  fs.readFile(path.join(__dirname, '/code/', filename), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('An error occurred while reading heatmap.js');
+      return res.status(500).send(`An error occurred while reading ${filename}`);
     }
     const replacedData = data.replace('MAPBOX_API', process.env.MAPBOX_API);
     res.setHeader('Content-Type', 'application/javascript');
